9bb278e2c50796058398ceb9886749d0b15a57e7
smalldirtybird <smalldirtybird@gmail.com>
2022-08-09 15:55:57 +0300

    Move text into files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 from functools import partial
+from pprint import pprint
 from textwrap import dedent
 
 import redis
@@ -23,7 +24,10 @@ def get_database_connection(database_password, database_host, database_port):
     return database
 
 
-def send_main_menu_keyboard(bot, chat_id):
+def send_main_menu_keyboard(bot, chat_id, course_content_folder):
+    filepath = os.path.join(course_content_folder, 'main_header.txt')
+    with open(filepath, 'r') as main_header_file:
+        main_header = main_header_file.read()
     keyboard = [
         [InlineKeyboardButton('Для кого', callback_data='symptoms')],
         [InlineKeyboardButton('Программа', callback_data='program')],
@@ -32,17 +36,13 @@ def send_main_menu_keyboard(bot, chat_id):
     ]
     bot.send_message(
         chat_id=chat_id,
-        text=dedent(
-            """
-            Анатомия стыда и вины.
-            Способ справиться с негативными переживаниями.
-            """),
+        text=main_header,
         reply_markup=InlineKeyboardMarkup(keyboard),
     )
 
 
-def start(bot, update):
-    send_main_menu_keyboard(bot, update['message']['chat_id'])
+def start(bot, update, course_content_folder):
+    send_main_menu_keyboard(bot, update['message']['chat_id'], course_content_folder)
     return 'HANDLE_MENU'
 
 
@@ -57,13 +57,12 @@ def send_program_info(bot, chat_id, course_content_folder):
             InlineKeyboardButton('В меню', callback_data='back_to_menu'),
             InlineKeyboardButton('О преподавателях', callback_data='mentors')
         ])
+    filepath = os.path.join(course_content_folder, 'program_general_info.txt')
+    with open(filepath, 'r') as program_general_info_file:
+        program_general_info = program_general_info_file.read()
     bot.send_message(
         chat_id=chat_id,
-        text=dedent("""
-Курс состоит из 5 занятий в формате видео-лекций длительностью от 30 до 60 минут.
-
-После каждого урока вы получите домашнее задание для практической отработки. В пакет курса включены специальные бланки и аудиозаписи для практической части.
-        """),
+        text=dedent(program_general_info),
         reply_markup=InlineKeyboardMarkup(keyboard),
     )
 
@@ -72,7 +71,7 @@ def handle_symptoms(bot, update, course_content_folder):
     query = update.callback_query.data
     chat_id = update.callback_query['message']['chat_id']
     if query == 'back_to_menu':
-        send_main_menu_keyboard(bot, chat_id)
+        send_main_menu_keyboard(bot, chat_id, course_content_folder)
         return 'HANDLE_MENU'
     if query == 'program':
         send_program_info(bot, chat_id, course_content_folder)
@@ -96,18 +95,20 @@ def get_lessons_navigation_menu(query):
     return keyboard
 
 
-def send_mentors_menu(bot, chat_id):
-    keyboard = [
-        [InlineKeyboardButton('Ольга Пичугина', callback_data='mentor_1')],
-        [InlineKeyboardButton('Анастасия Демченко', callback_data='mentor_2')],
-        [InlineKeyboardButton('В меню', callback_data='back_to_menu')]]
+def send_mentors_menu(bot, chat_id, course_content_folder):
+    lessons_content_filepath = os.path.join(course_content_folder, 'mentors.json')
+    keyboard = []
+    with open(os.path.normpath(lessons_content_filepath), 'r') as mentors_json:
+        mentors = json.load(mentors_json)
+        for mentor, content in mentors.items():
+            keyboard.append([InlineKeyboardButton(content['name'], callback_data=mentor)])
+    keyboard.append([InlineKeyboardButton('В меню', callback_data='back_to_menu')])
+    filepath = os.path.join(course_content_folder, 'mentors_header.txt')
+    with open(filepath, 'r') as mentors_header_file:
+        mentors_header = mentors_header_file.read()
     bot.send_message(
         chat_id=chat_id,
-        text=dedent("""
-Преподаватели
-
-Мы работали над данным курсом более полугода, чтобы вы могли лучше разобраться в непростых эмоциональных переживаниях 
-        """),
+        text=dedent(mentors_header),
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
@@ -122,10 +123,10 @@ def handle_program(bot, update, course_content_folder):
         for lesson, content in lessons.items():
             lessons_content[lesson] = f'{content["header"]}\n{content["content"]}'
     if query == 'back_to_menu':
-        send_main_menu_keyboard(bot, chat_id)
+        send_main_menu_keyboard(bot, chat_id, course_content_folder)
         return 'HANDLE_MENU'
     if query == 'mentors':
-        send_mentors_menu(bot, chat_id)
+        send_mentors_menu(bot, chat_id, course_content_folder)
         return 'HANDLE_MENTORS'
     if query == 'program':
         send_program_info(bot, chat_id, course_content_folder)
@@ -139,44 +140,34 @@ def handle_program(bot, update, course_content_folder):
     return 'HANDLE_PROGRAM'
 
 
-def handle_mentors(bot, update):
+def handle_mentors(bot, update, course_content_folder):
     query = update.callback_query.data
     chat_id = update.callback_query['message']['chat_id']
-    mentors = {
-        'mentor_1': """
-Ольга Пичугина
-
-Клинический психолог, член АКБТ, психолог Центра когнитивной терапии, организатор и преподаватель образовательной программы по КПТ в Москве и регионах
-
-Благодаря Ольге наш курс приобрел четкую и логичную структуру, доступную каждому, а также яркую и емкую презентацию. Профессиональные интересы Ольги лежат в области работы с депрессивным и тревожными расстройствами, обсессивно-компульсивным расстройством. Направления работы: когнитивно-поведенческая терапия, схема-терапия.        
-        """,
-        'mentor_2': """
-Анастасия Демченко
-
-Клинический психолог, психолог Центра когнитивной терапии, преподаватель образовательной программы по КПТ в Москве и регионах
-
-Анастасия являлась идейным вдохновителем создания данного курса. Профессиональные интересы Анастасии лежат в области работы с депрессивным и тревожными расстройствами, обсессивно-компульсивным расстройством. Направления работы: когнитивно-поведенческая терапия, схема-терапия, терапия принятием и ответственностью.
-        """
-    }
+    lessons_content_filepath = os.path.join(course_content_folder, 'mentors.json')
+    mentors_content = {}
+    with open(os.path.normpath(lessons_content_filepath), 'r') as mentors_json:
+        mentors = json.load(mentors_json)
+        for mentor, content in mentors.items():
+            mentors_content[mentor] = f'{content["name"]}\n{content["description"]}'
     if query == 'back_to_menu':
-        send_main_menu_keyboard(bot, chat_id)
+        send_main_menu_keyboard(bot, chat_id, course_content_folder)
         return 'HANDLE_MENU'
     if query == 'mentors':
-        send_mentors_menu(bot, chat_id)
-    if query in mentors.keys():
+        send_mentors_menu(bot, chat_id, course_content_folder)
+    if query in mentors_content.keys():
         bot.send_message(
             chat_id=chat_id,
-            text=dedent(mentors[query]),
+            text=dedent(mentors_content[query]),
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Назад', callback_data='mentors')]])
         )
     return 'HANDLE_MENTORS'
 
 
-def handle_payment(bot, update):
+def handle_payment(bot, update, course_content_folder):
     query = update.callback_query.data
     chat_id = update.callback_query['message']['chat_id']
     if query == 'back_to_menu':
-        send_main_menu_keyboard(bot, chat_id)
+        send_main_menu_keyboard(bot, chat_id, course_content_folder)
         return 'HANDLE_MENU'
 
 
@@ -191,21 +182,12 @@ def handle_menu(bot, update, course_content_folder):
                 InlineKeyboardButton('К программе', callback_data='program')
             ]
         ]
+        filepath = os.path.join(course_content_folder, 'symptoms.txt')
+        with open(filepath, 'r') as symptoms_file:
+            symptoms = symptoms_file.read()
         bot.send_message(
             chat_id=chat_id,
-            text=dedent('''Последствия этих эмоций могут выражаться в:
-        
-- постоянном сравнении себя с другими, постоянной самокритике
-- непокидающее фоновое ощущение собственной дефективности
-- сниженная самооценка
-- проблемы с границами, неумение сказать нет, взятие на себя чрезмерной ответсвенности за других
-            
-Если вы заметили, что один или больше пунктов про вас, и это в значительной мере ухудшает качество вашей жизни, данный курс может стать хорошим первым шагом для работы с эмоциями стыда и вины.
-        
-Данный курс построен на принципах доказательной психотерапии. Мы собрали материалы более 10 книги последних исследований в одном месте.
-
-Вы узнаете, чем отличаются вина и стыд, откуда они взялись именно у вас, научитесь определять, когда они адаптивны, а когда нет. И самое главное - получите практические инструменты для самостоятельной работы с ними.         
-        '''),
+            text=dedent(symptoms),
             reply_markup=InlineKeyboardMarkup(keyboard),
         )
         return 'HANDLE_SYMPTOMS'
@@ -213,15 +195,15 @@ def handle_menu(bot, update, course_content_folder):
         send_program_info(bot, chat_id, course_content_folder)
         return 'HANDLE_PROGRAM'
     if query == 'mentors':
-        send_mentors_menu(bot, chat_id)
+        send_mentors_menu(bot, chat_id, course_content_folder)
         return 'HANDLE_MENTORS'
     if query == 'get_course':
         bot.send_message(
             chat_id=chat_id,
             text=dedent("""
-Чтобы заказать курс, пожалуйста, пройдите по сслыке:
-https://kochet-psy.ru/anatomy_of_emotions
-"""),
+                Чтобы заказать курс, пожалуйста, пройдите по сслыке:
+                https://kochet-psy.ru/anatomy_of_emotions
+                """),
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('В меню', callback_data='back_to_menu')]])
         )
         return 'HANDLE_PAYMENT'
@@ -250,12 +232,12 @@ def handle_users_reply(bot, update, database_password, database_host,
     else:
         user_state = db.get(tg_user_id).decode('utf-8')
     states_functions = {
-        'START': start,
+        'START': partial(start, course_content_folder=course_content_folder),
         'HANDLE_MENU': partial(handle_menu, course_content_folder=course_content_folder),
         'HANDLE_SYMPTOMS': partial(handle_symptoms, course_content_folder=course_content_folder),
         'HANDLE_PROGRAM': partial(handle_program, course_content_folder=course_content_folder),
-        'HANDLE_MENTORS': handle_mentors,
-        'HANDLE_PAYMENT': handle_payment,
+        'HANDLE_MENTORS': partial(handle_mentors, course_content_folder=course_content_folder),
+        'HANDLE_PAYMENT': partial(handle_payment, course_content_folder=course_content_folder),
     }
     state_handler = states_functions[user_state]
     next_state = state_handler(bot, update)
